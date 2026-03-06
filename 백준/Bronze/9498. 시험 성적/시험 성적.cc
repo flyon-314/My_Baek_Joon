60c8884b05d84a88d6991da75bec7fda6942384c
Flyon <flyon314@gmail.com>
2026-03-06 21:54:32 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.cc
+++ 백준/Bronze/9498. 시험 성적/시험 성적.cc
@@ -1,13 +1,18 @@
-#include <bits/stdc++.h>
-
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+#include <math.h>
 using namespace std;
-int main(){
-    int a;
-    cin>>a;
-    if(a<=100 && a>=90) cout<<"A";
-    else if(a>=80) cout<<"B";
-    else if(a>=70) cout<<"C";
-    else if(a>=60) cout<<"D";
-    else cout<<"F";
 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); 
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    cout << (n >= 90 ? 'A' : n >= 80 ? 'B' : n >= 70 ? 'C' : n >= 60 ? 'D' : 'F');
+    return 0;
 }
