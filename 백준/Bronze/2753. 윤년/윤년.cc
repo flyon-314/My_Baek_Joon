c5d8f653126bb5f03c8931f34a3572fd305b2509
Flyon <flyon314@gmail.com>
2026-03-06 21:15:42 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.cc
+++ 백준/Bronze/2753. 윤년/윤년.cc
@@ -1,9 +1,17 @@
-#include <bits/stdc++.h>
-
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+#include <math.h>
 using namespace std;
-int main(){
-    int year;
-    cin>>year;
-    if((year%4==0&&year%100!=0)||year%400==0) cout<<1;
-    else cout<<0;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); 
+    cout.tie(NULL);
+    int n;
+    cin >> n;
+
+    cout << ((n % 4 == 0 && (n % 100 != 0 || n % 400 == 0)) ? 1 : 0) ;
+    return 0;
 }
