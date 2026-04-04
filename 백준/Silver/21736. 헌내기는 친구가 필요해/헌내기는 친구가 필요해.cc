8947c363f8b6acaccb892811835084ab375c46a1
Flyon <flyon314@gmail.com>
2026-04-05 00:10:03 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 8 ms, Memory: 2992 KB -BaekjoonHub

--- 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.cc
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.cc
@@ -1,61 +1,58 @@
-#include <algorithm>
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+#include <math.h>
 #include <queue>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <set>
 #include <string>
 #include <unordered_map>
-#include <vector>
+#include <climits>
+#include <unordered_set>
+
 using namespace std;
 
-int dx[4] = {0 , -1 , 0 ,1};
-int dy[] = {-1 , 0 , 1 , 0};
-int visited[601][601];
-int main(   )
+char campus[601][601];
+bool visited[601][601];
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, 1, 0, -1};
+int main()
 {
-    ios_base::sync_with_stdio();
-    cin.tie(NULL);
-    cout.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-    int N, M;
-    cin >> N >> M;
-    vector<string> grid(N);
-    queue <pair<int,int>> q;
-    for (int i = 0; i < N; i++)
-    {
-        string str;
-        cin >> str;
-        grid[i] = str;
+    int n, m;
+    cin >> n >> m;
+    queue <pair <int, int> > q;
+    
+    for (int i = 0; i < n; i++) {
+        string s;
+        cin >> s;
+        for (int j = 0 ; j < s.length(); j++) {
+            campus[i][j] = s[j];
+            if (s[j] == 'I') {
+                q.push({i, j});
+                visited[i][j] = true;
+            }
+        }    
     }
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < M; j++)
-        {
-           if (grid[i][j] == 'I')
-               q.push(make_pair(i,j));
-        }
-    }    
-    int count = 0;
-    while (!q.empty())
-    {
-        pair<int,int> p = q.front();
+
+    int anw = 0;
+    while (!q.empty()) {
+        pair<int, int> p = q.front();
         q.pop();
+        if (campus[p.first][p.second] == 'P') anw++;
+        for (int k = 0; k < 4; k++) {
+            int nx = p.second + dx[k];
+            int ny = p.first + dy[k];
 
-        for (int i = 0; i < 4; i++)
-        {
-            int ny = p.first + dy[i];
-            int nx = p.second + dx[i];
-            
-            if (ny < 0 || nx < 0 || ny >= N || nx >= M) continue;
-            if (grid[ny][nx] == 'X') continue;
-            if (visited[ny][nx]) continue;
-            if (grid[ny][nx] == 'P') count ++;
-            visited[ny][nx] = 1;
-            q.push(make_pair(ny,nx));
-            
+            if (nx < 0 || ny < 0 || nx >= m || ny >= n) continue;
+            if (campus[ny][nx] == 'X' || visited[ny][nx]) continue;
+            visited[ny][nx] = true;
+            q.push({ny, nx});
         }
     }
-    if (count == 0) cout << "TT\n";
-    else cout << count << "\n";
-}
+    if (anw == 0) cout << "TT" << endl;
+    else cout << anw ;
+    return 0;
+}   
