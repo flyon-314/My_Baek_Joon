d9f84b44294eef24066780b1ca5e44b937522410
Flyon <flyon314@gmail.com>
2026-04-21 20:29:23 +0900

    [Bronze V] Title: CAPS, Time: 4 ms, Memory: 3680 KB -BaekjoonHub

---
+++ 백준/Bronze/15000. CAPS/CAPS.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    string s;
+    cin >> s;
+
+    for (char &c : s) {
+        if (c >= 'a' && c <= 'z') {
+            c = c - 'a' + 'A';
+        }
+    }
+
+    cout << s << "\n";
+
+    return 0;
+}
